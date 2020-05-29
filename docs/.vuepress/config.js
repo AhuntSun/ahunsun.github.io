bfb25dc7cb9376e3be2c91bed266c17a290a22b5
ahuntsun <ahuntsun@163.com>
2020-05-29 21:01:51 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: '你好 VuePress',
-  description: '钊哥带你上王者',
-  dest: './dist',
+  title: '你好 VuePress',//博客标题
+  description: '钊哥带你上王者',//副标题
+  dest: './dist',//配置执行build命令后输出的目录，默认目录为.vuepress
   port: '7777',
   head: [
     ['link', {rel: 'icon', href: './logo.jpg'}],
@@ -11,18 +11,21 @@ module.exports = {
     lineNumbers: true
   },
   themeConfig: {
+    //导航栏
     nav: require("./nav.js"),
+    //侧边栏
     sidebar: require("./sidebar.js"),
-sidebarDepth: 2,
-lastUpdated: 'Last Updated',
-searchMaxSuggestoins: 10,
-serviceWorker: {
-  updatePopup:{
-    message: "有新内容",
-    buttonText: "更新",
-  }
-},
-editLinks: true,
-editLinksText: "在Github 上编辑此页！"
+    //其他参数配置
+    sidebarDepth: 4,
+    lastUpdated: 'Last Updated',
+    searchMaxSuggestoins: 10,
+    serviceWorker: {
+      updatePopup:{
+        message: "有新内容",
+        buttonText: "更新",
+      }
+    },
+    editLinks: true,
+    editLinksText: "在Github 上编辑此页！"
   }
 }
--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,14 +1,17 @@
 module.exports =[
+  //深度为1的导航栏
   {
     text: "懵逼指南",link: '/guide/'//!这里的连接会自动显示guide下的README.md文件
   },
+  //深度为2的导航栏
   {
     text: "面试宝典",//!去掉link表示不让点（有子导航标签的时候一般不让点）
     items: [
-      {text: '初级开发篇', link: '/baodian/zero/'},
+      {text: '初级开发篇', link: '/baodian/zero/'},//外链的形式有两种
       {text: '中高进阶篇', link: '/baodian/high/'},
     ]
   },
+  //深度为3的导航栏
   {
     text: '工具箱',
     items: [
