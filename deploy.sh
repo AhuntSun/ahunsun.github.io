0596ee468222336ddc6048a264a1cadea695f3a1
ahuntsun <ahuntsun@163.com>
2020-05-29 21:59:00 +0800

    搭建完成

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,25 @@
-set e
-
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
-
-# 绑定自定义域名
-echo 'ahuntsun.top' > CNAME
-
-git init
-git config user.name "ahuntsun"
-git config user.email "ahuntsun@163.com"
-git add -A
-git commit -m 'deploy-coding'
-
-# git@ahuntsun:ahuntsun/vuepress_blog.git
-git push -f master
-cd -
+#!/usr/bin/env sh
+ 
+# 确保脚本抛出遇到的错误
+set -e
+ 
+# 生成静态文件
+npm run build
+ 
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+ 
+# 如果是发布到自定义域名
+echo 'ahuntsun.com' > CNAME
+ 
+git init
+git add -A
+git commit -m 'deploy'
+ 
+# 如果你想要部署到 https://USERNAME.github.io
+# git push -f git@github.com:nan-gong/nan-gong.github.io.git master
+ 
+# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+ 
+cd -
