34bd88694bc51709526efcb9df219827d1948bc1
ahuntsun <ahuntsun@163.com>
2020-08-21 09:33:50 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,19 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+#切换到dev分支
+git checkout dev
+
+#将master分支合并到dev分支
+git merge master
+
+# 将原项目推送到dev分支
+git add -A
+
+git commit -m 'update'
+
+git push -f git@github.com:AhuntSun/ahunsun.github.io.git dev
+
 # 生成静态文件
 npm run docs:build
 
